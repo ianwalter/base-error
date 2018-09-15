3327d789142d53bf607180839f23e0902e7efe36
Ian Walter <public@iankwalter.com>
2018-09-14 21:38:17 -0400

    Turning off microbundle compression

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 module.exports = class BaseError extends Error {
   constructor (err) {
     super(err)
-    this.name = this.constructor.name
     if (err instanceof Error) {
       this.original = err
       this.stack = err.stack
@@ -12,5 +11,6 @@ module.exports = class BaseError extends Error {
       }
       this.message = err
     }
+    this.name = this.constructor.name
   }
 }
